ad92ab5ffb4077f86ee55534b2013a773426fc48
Ashwin <ashwin.witbooi@gmail.com>
2024-08-04 12:00:15 +0200

    initial commit bare project

---
+++ src/main/java/za/co/ashtech/parkgr/ParkgrApplication.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.parkgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/ashtech/parkgr/ParkgrApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.parkgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
