c6024f238692f5a5933d129c70814729617dd613
Ashwin <ashwin.witbooi@gmail.com>
2024-08-08 17:01:20 +0200

    update test use mock to test unit

--- src/test/java/za/co/ashtech/parkgr/unit/db/ParkrunRepositoryTests.java
+++ src/test/java/za/co/ashtech/parkgr/unit/db/ParkrunRepositoryTests.java
@@ -1,28 +1,43 @@
 package za.co.ashtech.parkgr.unit.db;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import za.co.ashtech.parkgr.db.entities.Parkrun;
 import za.co.ashtech.parkgr.db.repository.ParkrunRepository;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ParkrunRepositoryTests {
-	
-	@Autowired
+
+	@Mock
 	ParkrunRepository parkrunRepository;
 
 	@BeforeEach
 	void verifyConfig() {
 		assertNotNull(parkrunRepository);
+		
+		Parkrun parkrun = new Parkrun("Belhar", "Adam Tas Street", "1", "Belhar", "Cape Town", "WC");
+		Parkrun parkrun1 = new Parkrun("Brackenfell", "Old Paarl Road", "111", "Brackenfell", "Cape Town", "WC");
+		
+		List<Parkrun> parkruns = Arrays.asList(parkrun,parkrun1);
+		// When
+		when(parkrunRepository.findAll()).thenReturn(parkruns);
 	}
-	
+
+
 	@Test
 	void getRandomParkrunTest() {
-		assertNotNull(parkrunRepository.findAll());
+		assertEquals(true, parkrunRepository.findAll().iterator().hasNext());
 	}
 
 }
