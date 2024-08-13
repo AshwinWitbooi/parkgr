41fcc9cff3b412a7c567e0799e19fc8177296a90
Ashwin <ashwin.witbooi@gmail.com>
2024-08-13 21:06:14 +0200

    add integration test for get random park run

--- src/test/java/za/co/ashtech/parkgr/ParkgrApplicationTests.java
+++ src/test/java/za/co/ashtech/parkgr/ParkgrApplicationTests.java
@@ -1,13 +1,41 @@
 package za.co.ashtech.parkgr;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.net.URISyntaxException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import za.co.ashtech.parkgr.controller.models.RandomResource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkgrApplicationTests {
+	
+    @LocalServerPort
+    private int port;
+    
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
 	@Test
-	void contextLoads() {
+	void testGetRandomParkRun() throws URISyntaxException {		
+	    // Set up the headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("txId", TestUtils.testUUID());
+        headers.set("senderId", TestUtils.SENDER_ID);
+        headers.set("externalRef", TestUtils.externalRef());
+        
+        // Create an HttpEntity with headers
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        // Send the request with headers
+        ResponseEntity<RandomResource> response = testRestTemplate.exchange("http://localhost:"+port+"/parkgr/parkrun/random", HttpMethod.GET, entity, RandomResource.class);
+        assertNotNull(response.getBody().getParkrun());
+	
 	}
-
 }
